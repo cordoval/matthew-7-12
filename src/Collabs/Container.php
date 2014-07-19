fbbcc978f1f21dda5037c79368d8c3f1acb9ee09
Luis Cordova <cordoval@gmail.com>
2014-07-19 01:42:55 -0500

    start implementing clone and checkout

--- src/Collabs/Container.php
+++ src/Collabs/Container.php
@@ -2,20 +2,32 @@
 
 namespace Grace\Collabs;
 
+use Grace\Domain\Repo;
+
 class Container
 {
-    public function __construct()
+    protected $gitHelper;
+
+    public function __construct($gitHelper)
     {
-
-    }
-
-    public function checkout(Repo $repo, $reference)
-    {
-
+        $this->gitHelper = $gitHelper;
     }
 
     public function gitClone(Repo $repo)
     {
+        $vendor = $repo->getHookPost()->getVendor();
+        $name = $repo->getHookPost()->getName();
+        $baseUrl = 'github.com';
+        $command = sprintf('git clone git@%s:%s/%s.git', $baseUrl, $vendor, $name);
+        $cwd = uniqid('container_folder_');
+        $this->gitHelper->run($command, $cwd);
+        $repo->wasClonedIn($cwd);
+    }
+
+    public function checkout(Repo $repo, $reference)
+    {
+        $command = sprintf('git checkout -b %s -f', $reference);
+        $this->gitHelper->run($command, $cwd);
 
     }
 
--- src/Collabs/ContainerAwareTrait.php
+++ src/Collabs/ContainerAwareTrait.php
@@ -4,9 +4,10 @@ namespace Grace\Collabs;
 
 trait ContainerAwareTrait
 {
+    /** @var Container */
     protected $container;
 
-    public function setContainer(Container $container)
+    public function __construct(Container $container)
     {
         $this->container = $container;
     }
