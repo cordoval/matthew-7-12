c35f576de75d1d44067a94321982ed50f5c566d9
rajesh <rajesh@soarlogic.com>
2014-07-22 11:48:14 +0530

    working on push code test

--- src/UseCases/PushCodeTest.php
+++ src/UseCases/PushCodeTest.php
@@ -1,14 +1,55 @@
 <?php
 
 namespace Grace\UseCases;
+use Symfony\Component\HttpFoundation\Request;
+use Grace\Domain\Change;
+use Grace\Domain\Repo;
+use Grace\Domain\Branch;
+use Grace\Domain\PullRequest;
 
+/**
+ *
+ */
 class PushCodeTest extends BaseProphecy
 {
     /**
-     * @test
+     * @var
      */
-    public function it_goes_through_the_whole_pull_flow()
-    {
+    protected $patcher;
 
+    /**
+     * @var
+     */
+    protected $receiver;
+
+    /**
+     * @var
+     */
+    protected $usherer;
+
+    /**
+     * @var
+     */
+    protected $container;
+
+    public function setUp()
+    {
+        $this->container = static::$kernel->getContainer();
+        $this->patcher = $this->container->get('grace.patcher');
+        $this->receiver = $this->container->get('grace.receiver');
+        $this->usherer = $this->container->get('grace.usherer');
+    }
+
+    public function it_goes_through_the_whole_push_flow(Request $request)
+    {
+        $this->setUp();
+        $change = Change::from($request);
+        $changeSet = $this->receiver($change);
+        $repoChangeSet = Repo::from($changeSet);
+        $clonedRepository = $this->container($repoChangeSet);
+        $patch = Branch::toPatch($changeSet, $clonedRepository);
+        $patchedBranch = $this->patcher($patch);
+        $pullPatchedBranch = PullRequest::from($patchedBranch);
+        $this->usherer($pullPatchedBranch);
     }
 }
