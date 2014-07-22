b6b0758a64a23a91c3c8e364d021107cf23b45e1
Luis Cordova <cordoval@gmail.com>
2014-07-22 11:25:59 -0500

    plug process arguments method

--- src/Collabs/Helper.php
+++ src/Collabs/Helper.php
@@ -8,6 +8,10 @@ class Helper
 {
     public function run($command, $allowFailures = true)
     {
+        if (is_string($command)) {
+            $command = $this->parseProcessArguments($command);
+        }
+
         $builder = new ProcessBuilder($command);
         $builder
             ->setWorkingDirectory(getcwd())
@@ -20,5 +24,33 @@ class Helper
         if (!$process->isSuccessful() && !$allowFailures) {
             throw new \RuntimeException($process->getErrorOutput());
         }
+    }
+
+    /**
+     * @param string $command
+     *
+     * @return string[]
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function parseProcessArguments($command)
+    {
+        if (preg_match_all('/((?:"(?:(?:[^"\\\\]|\\\\.)+)")|(?:\'(?:[^\'\\\\]|\\\\.)+\')|[^ ]+)/i', $command, $args)) {
+            $normalizeCommandArgument = function ($argument) {
+                if ("'" === $argument[0] || '"' === $argument[0]) {
+                    $quote = $argument[0];
+
+                    $argument = substr($argument, 1, -1);
+                    $argument = str_replace('\\'.$quote, $quote, $argument);
+                    $argument = str_replace('\\\\', '\\', $argument);
+                }
+
+                return $argument;
+            };
+
+            return array_map($normalizeCommandArgument, $args[0]);
+        }
+
+        throw new \InvalidArgumentException(sprintf('Unable to parse command "%s".', $command));
     }
 }
