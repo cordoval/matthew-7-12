4203969dc3bfaaee3254ed490cf1795d557e8d16
Luis Cordova <cordoval@gmail.com>
2014-07-22 11:17:49 -0500

    sketch helper

--- src/Collabs/Helper.php
+++ src/Collabs/Helper.php
@@ -2,10 +2,23 @@
 
 namespace Grace\Collabs;
 
+use Symfony\Component\Process\ProcessBuilder;
+
 class Helper
 {
-    public function run()
+    public function run($command, $allowFailures = true)
     {
+        $builder = new ProcessBuilder($command);
+        $builder
+            ->setWorkingDirectory(getcwd())
+            ->setTimeout(3600)
+        ;
+        $process = $builder->getProcess();
 
+        $process->run();
+
+        if (!$process->isSuccessful() && !$allowFailures) {
+            throw new \RuntimeException($process->getErrorOutput());
+        }
     }
 }
