5ef9ec6d2d033dc20e6f8d3ec9fd916dab7d6c23
Yamir Ramirez <ysramirez@gmail.com>
2014-08-07 12:13:31 -0500

    just a probe

--- src/UseCases/PushCodeTest.php
+++ src/UseCases/PushCodeTest.php
@@ -8,48 +8,53 @@ use Grace\Domain\Repo;
 
 class PushCodeTest extends WebTestCase
 {
-    protected $poller;
-    protected $downloader;
-    protected $unzipper;
-    protected $usherer;
     protected $container;
+    protected $puller;
+    protected $differ;
+    protected $subscriber;
+    protected $mailer;
+    protected $zipper;
+    protected $from;
+    protected $baseMailer;
+    protected $client;
 
     public function setUp()
     {
-        $this->container = static::$kernel->getContainer();
-//        $this->poller = $this->container->get('grace.poller');
-//        $this->downloader = $this->container->get('grace.downloader');
-//        $this->unzipper = $this->container->get('grace.unzipper');
-//        $this->usherer = $this->container->get('grace.usherer');
+        $this->client = $this->createClient();
+        $this->client->insulate();
+        $this->client->enableProfiler();
+
+        $container = static::$kernel->getContainer();
+        $this->puller = $container->get('grace.puller');
+        $this->differ = $container->get('grace.differ');
+        $this->subscriber = $container->get('grace.subscriber');
+        $this->mailer = $container->get('grace.mailer');
+        $this->zipper = $container->get('grace.zipper');
+        $this->from = $container->getParameter('from');
+        $this->baseMailer = $container->get('swiftmailer.mailer');
+        $this->container = $container->get('grace.container');
     }
 
     /**
      * @test
+     * @dataProvider getRequestExamples
      */
-    public function it_goes_through_the_whole_push_flow(Request $request)
+    public function it_goes_through_the_whole_pull_flow(Request $request)
     {
-        $this->markTestIncomplete('it needs finishing up');
-        /**
-         * patch via email is sent zipped
-         * check email with php imap specified email account for an attachment
-         * subject resolver : resolves email subject format (for now we assume they are always just one email)
-         * download attachment -> unzip = file.patch
-         * create repo & apply patch file.patch
-         * given appropriate name push to fork
-         * matthew account in github must be used - ssh key to be able to push to its own fork
-         * open PR with library api
-         */
-        $gotEmail = Poller::pollFromNotification($request);
-        $zipped = $this->downloader->__invoke($gotEmail);
-        $patch = $this->unzipper->__invoke($zipped);
-        $repo = Repo::fromPatch($patch);
-        $this->usherer->__invoke($repo, $repo->to);
+        $hookPost = GithubPost::fromRequest($request);
+        $repo = $this->puller->__invoke(Repo::fromHook($hookPost));
+        $patches = $this->differ->__invoke($repo);
+        $manyCompressed = $this->zipper->__invoke($patches);
+        $list = $this->subscriber->__invoke($repo);
+        $this->mailer->__invoke($list, $manyCompressed, $this->from, $this->baseMailer);
+        $this->container->destroy($repo);
     }
 
     public function getRequestExamples()
     {
         return [
-            [$this->getRequest(file_get_contents(__DIR__.'/Fixtures/requestpush.json'))],
+            [$this->getRequest(file_get_contents(__DIR__.'/Fixtures/request.json'))],
+//            [$this->getRequest(file_get_contents(__DIR__.'/Fixtures/request.json'))],
         ];
     }
 
