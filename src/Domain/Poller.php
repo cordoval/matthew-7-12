7f21ea6139b44c6128de8f941bac86975ab6a70e
Yamir Ramirez <ysramirez@gmail.com>
2014-08-07 21:40:44 -0500

    now we can read IMAP -- clean code

--- src/Domain/Poller.php
+++ src/Domain/Poller.php
@@ -6,11 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Poller extends \Horde_Imap_Client_Socket
 {
-    protected $email;
-    protected $from;
-    protected $to;
-    protected $name;
-
     public static function pollFromNotification($imapConnection)
     {
         return new self($imapConnection);
