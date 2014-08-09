d8ee68f4dac5480df4079130e04ac9ee50974ca8
Luis Cordova <cordoval@gmail.com>
2014-08-09 10:20:21 -0500

    bootstrap some more changes

--- src/UseCases/PushCodeTest.php
+++ src/UseCases/PushCodeTest.php
@@ -12,7 +12,7 @@ use Grace\Domain\Repo;
 class PushCodeTest extends WebTestCase
 {
     protected $poller;
-    protected $downloader;
+    protected $reader;
     protected $unzipper;
     protected $usherer;
     protected $container;
@@ -21,7 +21,7 @@ class PushCodeTest extends WebTestCase
     {
         $this->container = static::$kernel->getContainer();
         $this->poller = $this->container->get('grace.poller');
-        $this->downloader = $this->container->get('grace.downloader');
+        $this->reader = $this->container->get('grace.reader');
         $this->unzipper = $this->container->get('grace.unzipper');
         $this->usherer = $this->container->get('grace.usherer');
     }
@@ -31,16 +31,6 @@ class PushCodeTest extends WebTestCase
      */
     public function it_goes_through_the_whole_push_flow(Request $request)
     {
-        /**
-         * patch via email is sent zipped
-         * check email with php imap specified email account for an attachment
-         * subject resolver : resolves email subject format (for now we assume they are always just one email)
-         * download attachment -> unzip = file.patch
-         * create repo & apply patch file.patch
-         * given appropriate name push to fork
-         * matthew account in github must be used - ssh key to be able to push to its own fork
-         * open PR with library api
-         */
         $gotEmail = Poller::pollFromNotification($request);
         $zipped = $this->downloader->__invoke($gotEmail);
         $patch = $this->unzipper->__invoke($zipped);
