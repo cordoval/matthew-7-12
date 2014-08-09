3f95d06aecda58d73fa90a10b8155a58d594f08f
Yamir Ramirez <ysramirez@gmail.com>
2014-08-09 17:05:03 -0500

    moving the service to PushCode directory

--- src/PushCode/Reader.php
+++ src/PushCode/Reader.php
@@ -2,8 +2,20 @@
 
 namespace Grace\PushCode;
 
-class Reader
+use Ddeboer\Imap\Server;
+
+class Reader extends Server
 {
+    protected $username;
+    protected $password;
+
+    public function __construct($hostname, $username, $password, $port = 993, $flags = '/imap/ssl/validate-cert')
+    {
+        $this->username = $username;
+        $this->password = $password;
+        parent::__construct($hostname, $port, $flags);
+    }
+
     public function __invoke(Repo $repo)
     {
         return $repo;
