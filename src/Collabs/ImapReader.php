a0d55417a5297160af116a87d583693c1731bc65
Yamir Ramirez <ysramirez@gmail.com>
2014-08-09 16:12:14 -0500

    Initializing IMAP service - 1

--- src/Collabs/ImapReader.php
+++ src/Collabs/ImapReader.php
@@ -2,8 +2,18 @@
 
 namespace Grace\Collabs;
 
+use Ddeboer\Imap\Server;
 
 class ImapReader implements Imap
 {
+    protected $imapServer;
+    protected $username;
+    protected $password;
 
+    public function __construct($imapServer, $username, $password)
+    {
+        $this->imapServer = $imapServer;
+        $this->username = $username;
+        $this->password = $password;
+    }
 } 
