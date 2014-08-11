23dbf7f84ff3e11cf214bf79dabc24fe48cab634
Yamir Ramirez <ysramirez@gmail.com>
2014-08-11 18:19:29 -0500

    definig GitDriver class and grace.gitdriver service

---
+++ src/PushCode/GitDriver.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Grace\PushCode;
+
+
+class GitDriver {
+
+} 
--- src/UseCases/PushCodeTest.php
+++ src/UseCases/PushCodeTest.php
@@ -26,6 +26,7 @@ class PushCodeTest extends BaseTestCase
         $this->container = $container->get('grace.container');
         $this->unzipper = $container->get('grace.unzipper');
         $this->usherer = $container->get('grace.usherer');
+        $this->gitDriver = $container->get('grace.git.driver');
     }
 
     /**
@@ -35,8 +36,10 @@ class PushCodeTest extends BaseTestCase
     public function it_goes_through_the_whole_push_flow()
     {
         $projectName = 'INBOX';
-        $repoAndAttachment = $this->reader->__invoke($projectName);
-        $repoAndPatch = $this->unzipper->__invoke($repoAndAttachment);
+        $repoAndZipAttachment = $this->reader->__invoke($projectName);
+        $repoAndPatch = $this->unzipper->__invoke($repoAndZipAttachment);
+        $repoDriver = $this->gitDriver->createRepo($repoAndPatch);
+
         $repo = Repo::fromPatch($repoAndPatch);
         $this->usherer->__invoke($repo, $repo->to);
     }
