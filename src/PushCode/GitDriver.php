a73fa704e4a2ccf9a178e1869f425ab3c76f750d
Yamir Ramirez <ysramirez@gmail.com>
2014-08-11 18:36:13 -0500

    trying to clone we need a Repo object

--- src/PushCode/GitDriver.php
+++ src/PushCode/GitDriver.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace Grace\PushCode;
-
+use Grace\Collabs\ContainerAwareTrait;
 
 class GitDriver {
 
+    use ContainerAwareTrait;
+
+    public function createRepo($repoAndPatch)
+    {
+        $this->container->gitClone();
+    }
 } 
