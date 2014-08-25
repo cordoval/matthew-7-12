da7c1b6ef64e476ded88aa082500508e1c1e71df
Luis Cordova <cordoval@gmail.com>
2014-08-25 10:37:29 -0500

    wip

---
+++ src/ExampleSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Grace;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExampleSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+
+        ];
+    }
+}
